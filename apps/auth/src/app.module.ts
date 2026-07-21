6322c90c498723e5973e4d85815c029d176d44d1
Bop-bili-bop <pelmen1109@gmail.com>
2026-07-21 17:08:22 +0200

    setup auth service app module

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -1,9 +1,50 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+import {
+  appConfig,
+  databaseConfig,
+  jwtConfig,
+  kafkaConfig,
+  redisConfig,
+} from './config';
+
+import { validate } from './config/env.validation';
+
+import { LoggerModule } from './common/logger/logger.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      expandVariables: true,
+      envFilePath: '.env',
+      load: [appConfig, databaseConfig, jwtConfig, redisConfig, kafkaConfig],
+      validate,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.database'),
+
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+
+    LoggerModule,
+
+    AuthModule,
+  ],
 })
 export class AppModule {}
